f9877f63a2b9bba404e70ae2db0c408de8c71386
solita-michalguspiel <michal.guspiel@solita.fi>
2022-05-27 17:06:14 +0300

    Starting to work on notes list view.

--- iosApp/iosApp/ProfileView.swift
+++ iosApp/iosApp/ProfileView.swift
@@ -11,14 +11,16 @@ import shared
 
 
 class ProfileViewStateObject : ObservableObject{
-    
-
-    
+        
     var authViewModel = iosDI().getAuthViewModel()
 
     @Published var userState : AnyObject = ResponseEmpty()
-
     
+    @Published var userAuthState : AnyObject = ResponseEmpty()
+    
+    @Published var shouldPopBackStack : Bool = false
+
+
     init(){
         start()
     }
@@ -28,36 +30,62 @@ class ProfileViewStateObject : ObservableObject{
               let response = state! as Any
               self.userState = watchResponse(response: response)
         }
+        authViewModel.userAuthState.watch{state in
+            let response = state! as Any
+            self.userAuthState = watchResponse(response: response)
+            
+            if(self.userAuthState.isKind(of: ResponseSuccess<AnyObject>.self)){
+                let userAuthState = self.userAuthState as! ResponseSuccess<KotlinBoolean>
+                if(userAuthState.data == true){
+                   print("True, means signed in")
+                }
+                else{
+                    print("False, means signed out!")
+                    self.shouldPopBackStack = true
+                }
+            }
+        }
     }
     
 }
-
 
 struct ProfileView : View{
     
     @StateObject var stateObject = ProfileViewStateObject()
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
 
-    
     var body : some View {
+        
+    return TabView{
+        Text("This is going to show notes")
+        .tabItem{
+            Image(systemName: "house.fill")
+            Text("Notes")
+        }
+        
+        
+        ZStack{
         if(stateObject.userState.isKind!(of: ResponseSuccess<User>.self)){
             let user = (stateObject.userState as! ResponseSuccess<User>)
             VStack(spacing : 30){
-                
-                Text(user.data!.userEmail)
-                
+                Text("Hello " + user.data!.userEmail)
                 Button("Sign out"){
                     stateObject.authViewModel.signOut()
-                    presentationMode.wrappedValue.dismiss()
                 }
-            }.navigationBarBackButtonHidden(true)
+            }
         }
-    else{
-        Text("Loading...").onAppear(){
-            stateObject.authViewModel.getCurrentUserDocument()
+        else{
+            Text("Loading...").onAppear(){
+                stateObject.authViewModel.getCurrentUserDocument()
+            }
         }
+        if stateObject.shouldPopBackStack {
+            Text("").onAppear(){ presentationMode.wrappedValue.dismiss()}
+            }
+        }.tabItem{
+            Image(systemName: "person.fill")
+            Text("Profile ")
+        }
+    }.navigationBarBackButtonHidden(true)
     }
-    }
-
-
 }
